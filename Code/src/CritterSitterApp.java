b776b6849c2de0454bf22db81df24bc905297f67
LauraCool <cookiestrawberrygirl@gmail.com>
2015-05-17 16:53:48 -0400

    here is the new stuff jasmine with the new app and splashscreen class

--- Code/src/CritterSitterApp.java
+++ Code/src/CritterSitterApp.java
@@ -22,83 +22,31 @@ public class CritterSitterApp extends JFrame implements ActionListener
   
   /**
    * CritterSitterApp method is used to create the CritterSitterApp object in RAM while passing the String title, this also creates 3 menu items of Quit, Help and About which can be accessed through the 2 menus of File and Help respectively.It also adds the 5 items to the actual menu which is created and called 'myMenus'.Finally, this method sets the performance window to 400, 400 and visible to the user.It also ensures the window will closed when the user pushes the 'x' button.
-   * The first block of code creates the superclass object and passes the String title as well as creates the menu items and menu options and adds the quit, help and about menu items to the menu bar.
-   * The next block of code is used to create the actual menu bar, add the file and help menu options and make sure the help, quit and about menu items are being listened for, for user input.
-   * The last block of code is used to set the menu bar with the 400x400 frame, visible to the viewer and ensures it will close when the 'x' button is clicked. 
-   * 
-   * <p>
-   * <b>Local variables: </b>
-   * <p>
-   * <b>quitItem </b> This creates an instance of the JMenuItem class called "Quit".
-   * <p>
-   * <b>helpItem </b> This creates an instance of the JMenuItem class called "Help".
-   * <p>
-   * <b>aboutItem </b> This creates an instance of the JMenuItem class called "About".
-   * <p>
-   * <b>fileMenu </b> This creates an instance of the JMenu class called "File".
-   * <p>
-   * <b>helpMenu </b> This creates an instance of the JMenu class called "Help".
-   * <p>
-   * <b>myMenus </b> This creates an instance of the JMenuBar class.
    */
   public CritterSitterApp()
   {
     super ("Critter Sitter");
-    
-    JMenuItem quitItem = new JMenuItem ("Quit");
-    JMenuItem helpItem = new JMenuItem ("Help");
-    JMenuItem aboutItem = new JMenuItem ("About");
-    JMenu fileMenu = new JMenu ("File");
-    JMenu helpMenu = new JMenu ("Help");
-    fileMenu.add (quitItem);
-    helpMenu.add (helpItem);
-    helpMenu.add (aboutItem);
-    
-    JMenuBar myMenus = new JMenuBar ();
-    helpItem.addActionListener (this);
-    quitItem.addActionListener (this);
-    aboutItem.addActionListener (this);
-    myMenus.add (fileMenu);
-    myMenus.add (helpMenu);
-    
-    setJMenuBar (myMenus);
     setSize (780, 590);
     setVisible (true);
     setDefaultCloseOperation (JFrame.DISPOSE_ON_CLOSE);
     setResizable(false);
-    add(new DisplayScreen());
+//    add(new DisplayScreen());
+//    try 
+//    {
+//      Thread.sleep (100); 
+//    } 
+//    catch (Exception e) 
+//    {
+//    }
+//    repaint();
+    // ^above stuff works
+    add(new SplashScreen());
   }
   
   //do we need this method?
   public CritterSitterApp (String title, String label, int xSize)
   {
     dialog (title, label, xSize);
-  }
-  
-  /**
-   * This method is used when there is an action performed by the user through an input device. What happens is if the action command was "Quit" it will close the performance window, but if the action command was "Help" it would open a new performance window with a button with a different set of labels, titles and output size.Also if they choose "About" it will open a completely new performance window with a differen label (about game), button and size. 
-   * The if statements in this method are used in case there is an action performed from the user. If it is quit, help or about, it will each do a different thing.
-   *
-   * @param ae  This creates an instance of the ActionEvent class and helps to see if an action has been performed.
-   * 
-   * <p>
-   * <b>Local variables: </b>
-   */
-  public void actionPerformed (ActionEvent ae)
-  {    
-    if (ae.getActionCommand ().equals ("Quit"))
-    {
-      System.exit (0);
-    }
-    else if (ae.getActionCommand ().equals ("Help"))
-    {
-      dialog ("Help Me!", "Help Me!", 100);
-    }
-    else
-      if (ae.getActionCommand ().equals ("About"))
-    {
-      dialog ("About Game", "Here is some information regarding the game...", 750);
-    }
   }
   
   /**
