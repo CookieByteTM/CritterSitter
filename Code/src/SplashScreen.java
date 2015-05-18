5249900cd33e230a72984a029d9a157b4c6768dc
LauraCool <cookiestrawberrygirl@gmail.com>
2015-05-18 09:51:44 -0400

    iweioruwe
    i just changed the splash screen- its from yesterday - i fixed it! are
    you bringing your laptop tomorrow? cuz could you photoshop the pictures
    of the cookies on there?

--- Code/src/SplashScreen.java
+++ Code/src/SplashScreen.java
@@ -51,64 +51,106 @@ public class SplashScreen extends JPanel
       g.drawImage(image2, 0, 0,this);    
       if (x >= 200 && x < 550)
       {    
-        g.setColor (Color.RED);
-        g.fillRect(200, y-200, 300, 150);
-        animate(2);
+	g.setColor (Color.RED);
+	g.fillRect(200, y-200, 300, 150);
+	animate(2);
       }
       else if (x >= 550 && x < 800)
       {
-        g.setColor (Color.RED);
-        g.fillRect(200, (350 - (y-549)), 300, 150);
-        animate(2);
+	g.setColor (Color.RED);
+	g.fillRect(200, (350 - (y-549)), 300, 150);
+	animate(2);
       }
       else if (x >= 800 && x < 1050)
       {
-        g.setColor (Color.RED);
-        g.fillRect(200, (99 + (y-799)), 300, 150);
-        animate(1);
+	g.setColor (Color.RED);
+	g.fillRect(200, (99 + (y-799)), 300, 150);
+	animate(1);
       }
       else if (x >= 1050 && x < 1200)
       {
-        g.setColor (Color.RED);
-        g.fillRect(200, (350 - (y-1049)), 300, 150);
-        animate(1);
+	g.setColor (Color.RED);
+	g.fillRect(200, (350 - (y-1049)), 300, 150);
+	animate(1);
       }
       else if (x >= 1200 && x < 1350)
       {
-        g.setColor (Color.RED);
-        g.fillRect(200, (199 + (y-1199)), 300, 150);
-        animate(1);
+	g.setColor (Color.RED);
+	g.fillRect(200, (199 + (y-1199)), 300, 150);
+	animate(1);
       }
       else if (x >= 1350 && x < 1370)
       {
-        g.setColor (Color.RED);
-        g.fillRect(200, (350 - (y-1349)), 300, 150);
-        animate(5);
+	g.setColor (Color.RED);
+	g.fillRect(200, (350 - (y-1349)), 300, 150);
+	animate(5);
       }
       else if (x >= 1370 && x < 1380)
       {
-        g.setColor (Color.RED);
-        g.fillRect(200, (329 + (y-1369)), 300, 150);
-        animate(5);
+	g.setColor (Color.RED);
+	g.fillRect(200, (329 + (y-1369)), 300, 150);
+	animate(5);
       }
       else if(x >= 1380 && x < 1680)
       {
-        g.setColor (Color.RED);
-        g.fillRect(200, 340 - (y-1379), 300, 150);
-        animate(10);
+	g.setColor (Color.RED);
+	g.fillRect(200, 340 - (y-1379), 300, 150);
+	animate(10);
       }
       else
       {
-        g.setColor (Color.RED);
-        g.fillRect(200, 39, 300, 150);
-        g.fillRect(300, 230, 170, 170);
-        if (x >= 1680 && x <1780)
-        {
-          g.setColor (Color.BLACK);
-          g.setFont (new Font ("Helvetica", Font.BOLD, 25));
-          g.drawString("Loading" + (x-1679) + "%", 270, 350);
-          animate(2);
-        }
+	g.setColor (Color.RED);
+	g.fillRect(200, 39, 300, 150);
+	if (x >= 1680 && x <1780)
+	{
+	  g.setColor (Color.BLACK);
+	  g.setFont (new Font ("Helvetica", Font.BOLD, 25));
+	  g.drawString("Loading" + (x-1679) + "%", 270, 460);
+	  animate(2);
+	}
+	if (x >= 1680 && x < 1694)
+	    {
+	      Image cookie1 = new ImageIcon ("cookie1box.jpg").getImage();
+	      g.drawImage(cookie1, 300, 230,this);     
+	      animate(100);
+	    }
+//          else if (x >= 1694 && x < 1708)
+//          {
+//            Image cookie2= new ImageIcon ("MainMenuBackgroundBlue.jpg").getImage();
+//            g.drawImage(cookie2, 0, 0,this);      
+//            animate(100);
+//          }
+//          else if (x >= 1708 && x < 1722)
+//          {
+//            Image cookie3 = new ImageIcon ("MainMenuBackgroundBlue.jpg").getImage();
+//            g.drawImage(cookie3, 0, 0,this); 
+//            animate(100);
+//          }
+//          else if (x >= 1722 && x < 1736)
+//          {
+//            Image cookie4= new ImageIcon ("MainMenuBackgroundBlue.jpg").getImage();
+//            g.drawImage(cookie4, 0, 0,this);     
+//            animate(100);
+//          }       
+//          else if (x >= 1736 && x < 1750)
+//          {
+//            Image cookie5 = new ImageIcon ("MainMenuBackgroundBlue.jpg").getImage();
+//            g.drawImage(cookie5, 0, 0,this);    
+//            animate(100);
+//          }
+//          else if (x >= 1750 && x < 1764)
+//          {
+//            Image cookie6= new ImageIcon ("MainMenuBackgroundBlue.jpg").getImage();
+//            g.drawImage(cookie6, 0, 0,this);     
+//            animate(100);
+//          }   
+//          else
+//            if (x >= 1764 && x < 1780)
+//          {
+//            Image cookie7 = new ImageIcon ("MainMenuBackgroundBlue.jpg").getImage();
+//            g.drawImage(cookie7, 0, 0,this);        
+//            animate(100);
+//          }
       }
     }
   }
@@ -125,10 +167,10 @@ public class SplashScreen extends JPanel
     JButton calculate = new JButton ("Calculate");
     add (calculate);
     calculate.addActionListener (new ActionListener ()
-                                   {
+				   {
       public void actionPerformed (ActionEvent e)
       {
-        System.out.println ("YESSS!");
+	System.out.println ("YESSS!");
       }
     }
     );
